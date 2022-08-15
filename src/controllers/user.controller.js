bfa4395f0dfa6cf2fcdeac077a4d4f17bd2bf8bf
203426 <203426@ids.upchiapas.edu.mx>
2022-08-15 14:17:30 -0500

    Crud completed for user model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,7 +44,75 @@ const createUser = async (req, res) => {
     }
 }
 
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { username, email, password, lastname } = req.body
+        const target = await User.findByPk(id)
+
+
+        username ? (target.username = username) : null
+        email ? (target.email = email) : null
+        password ? (target.password = password) : null
+        lastname ? (target.lastname = lastname) : null
+
+        const result = await target.save()
+
+        res.json({
+            message: "user updated",
+            data: result
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Error updating data" })
+    }
+}
+
+
+const deleteUser = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const target = await User.findByPk(id);
+
+        const response = await target.destroy();
+
+        console.log(response);
+
+        res.json({
+            message: "User deleted corretly"
+        })
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+
+
+
+}
+
+
+const getSingleUSer = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const user = await User.findByPk(id)
+        user ?
+            res.json({
+                message: "User found",
+                data: user
+            })
+            : res.status(404).json({
+                message: "user not found"
+            })
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+}
+
 export {
     getUsers,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser,
+    getSingleUSer
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/user.controller.js";
+import { createUser, deleteUser, getSingleUSer, getUsers, updateUser } from "../controllers/user.controller.js";
 const router = Router();
 
 router.get('/user', getUsers)
 router.post('/user', createUser)
+router.put('/user/:id',updateUser)
+router.delete('/user/:id',deleteUser)
+router.get('/user/:id',getSingleUSer)
 
 
 export default router;
